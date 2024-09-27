b3a4e43a6f4a4fbfc145c8d39934d91dd477469a
prince6201 <155984611+prince6201@users.noreply.github.com>
2024-09-27 19:24:27 +0530

    Add files via upload

---
+++ reversestar.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i,j,n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    for (int i = n; i > 0; i--) 
+    {
+        for (int j = 1; j <= n; j++) 
+        {
+            if (j >= i) 
+            {
+                cout << "* ";
+            }
+            else 
+            {
+                cout << "  ";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
