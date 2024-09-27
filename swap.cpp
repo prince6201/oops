282aed07660c7ee49f7db1584bc40e1d8760aa0f
prince6201 <155984611+prince6201@users.noreply.github.com>
2024-09-27 19:25:26 +0530

    Add files via upload

---
+++ swap.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"Enter 1st number: ";
+    cin>>a;
+    cout<<"Enter 2nd number: ";
+    cin>>b;
+    c=a;
+    a=b;
+    b=c;
+    cout<<"First number: "<<a<< "\nSecond number: "<<b;
+    
+
+
+}
