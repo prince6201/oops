c2e01c878c880cf116949c298f898b7f5dab5c28
prince6201 <155984611+prince6201@users.noreply.github.com>
2024-09-27 19:34:52 +0530

    Add files via upload

---
+++ character.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j,n;
+    cout<<"Enter a number :";
+    cin>>n;
+    for(i=0;i<n;i++)
+    { 
+        char ch = 'A';
+        for(j=0;j<n;j++)
+        {
+            cout << ch<< " ";
+            ch = ch+1;
+        } 
+        cout << endl; 
+    }
+    
+    return 0;
+
+}
