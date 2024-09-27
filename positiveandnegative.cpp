b79764f576aff51af550280c1e540d8c3f481ce2
prince6201 <155984611+prince6201@users.noreply.github.com>
2024-09-27 19:17:26 +0530

    Add files via upload

---
+++ positiveandnegative.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int num;
+   cout<<"Enter a number : ";
+   cin>>num;
+   if(num>=0)
+     cout<<num<<"positive";
+   else
+     cout<<num<<"negative";
+
+   return 0;
+
+
+}
