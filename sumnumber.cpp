5906cf497328c45120afefa4463c6ab3f70ebcec
prince6201 <155984611+prince6201@users.noreply.github.com>
2024-09-27 19:25:01 +0530

    Add files via upload

---
+++ sumnumber.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,sum;
+    cout<<"Enter number a :";
+    cin>>a;
+    cout<<"Enter number b :";
+    cin>>b;
+    cout<<"sum = ";
+    sum = a+b;
+    cout<<sum;
+    return 0;
+
+}
