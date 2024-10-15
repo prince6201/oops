e11cb663c8f703256518e1f05cfb2f8c12fae0c4
prince6201 <155984611+prince6201@users.noreply.github.com>
2024-10-15 20:19:40 +0530

    Add files via upload

---
+++ funsum.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+double sumtwonum(double a,double b)
+{
+    double s = a+b;
+    return s;
+}
+int main()
+{
+    cout<<"sum = "<<sumtwonum(12.32, 14.42)<<endl;
+}
